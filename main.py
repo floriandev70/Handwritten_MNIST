a038a2787e53e92479f3a454186febe88cf58250
Florian <139658946+floriandev70@users.noreply.github.com>
2024-07-12 21:29:09 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+#########################################
+# main file to call the different models
+#########################################
+import Handwritten as hw
+print('start main')
+hw.handwritten()
